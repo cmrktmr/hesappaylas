c5edc08cfc80ad1fd484ace1827ea30b7da051f8
Cemre Aktemur <aktemur.cemre@gmail.com>
2023-10-12 18:29:28 +0300

    main page changed

--- components/Product.js
+++ components/Product.js
@@ -1,13 +1,23 @@
 import { useDrag } from 'react-dnd'
 
 export default function Product({ name, price }) {
-    const [, ref] = useDrag({
+    const [{ isDragging }, ref] = useDrag({
         type: 'PRODUCT',
         item: { name, price }
     });
 
+    const dragStyles = {
+        transition: 'all 0.3s ease',
+        boxShadow: isDragging ? '0px 4px 10px rgba(0,0,0,0.5)' : 'none',
+        borderColor: isDragging ? '#007BFF' : '#ccc',
+        backgroundColor: isDragging ? '#f8f9fa' : 'white',
+        borderRadius: '5px',
+        border: '2px solid',
+        color: isDragging ? '#007BFF' : '#000',
+    };
+
     return (
-        <div ref={ref}>
+        <div ref={ref} style={dragStyles}>
             {name} - {price} TL
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -4,64 +4,75 @@ import Avatar from '../components/Avatar';
 import Product from '../components/Product';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { TouchBackend } from 'react-dnd-touch-backend'
-import { useMemo } from 'react';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import Button from 'react-bootstrap/Button';
+import { useForm } from 'react-hook-form';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-  const [avatarProducts, setAvatarProducts] = useState({ avatar1: [], avatar2: [] });
+  const [avatarCount, setAvatarCount] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  const [avatarProducts, setAvatarProducts] = useState([]);
+  
   const isTouchDevice = typeof window !== 'undefined' && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
-  const backendForDND = isTouchDevice ? TouchBackend : HTML5Backend;
-console.log(isTouchDevice,"isTouchDevice")
+
+  const { register, handleSubmit } = useForm();
+  
+  const onCountSubmit = (data) => {
+    setAvatarCount(data.count);
+    setAvatarProducts(Array(Number(data.count)).fill([]));
+    setShowForm(true);
+  };
+
   const handleAddProduct = (product) => {
     setProducts([...products, product]);
   };
 
-  const handleProductDrop = (avatarName, product) => {
-    setAvatarProducts(prev => ({
-      ...prev,
-      [avatarName]: [...prev[avatarName], product]
-    }));
+  const handleProductDrop = (avatarIndex, product) => {
+    setAvatarProducts(prev => {
+      const newAvatars = [...prev];
+      newAvatars[avatarIndex] = [...newAvatars[avatarIndex], product];
+      return newAvatars;
+    });
   };
 
   return (
-    <div>
+    <div className="container mt-5">
       <h1>Hesabı Paylaşmaya Başla</h1>
-
-      <AddProductForm onAdd={handleAddProduct} />
-    <DndProvider backend={backendForDND}>
-
-        <div className="products">
+      {!showForm ? (
+        <form onSubmit={handleSubmit(onCountSubmit)}>
+          <input type="number" placeholder="Kişi sayısı" {...register("count")} />
+          <Button type="submit">Başla</Button>
+        </form>
+      ) : (
+        <>
+          <AddProductForm onAdd={handleAddProduct} />
+          <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
+        <div className="my-4">
           {products.map((product, index) => (
             <Product key={index} name={product.name} price={product.price} />
           ))}
         </div>
 
-        <div className="avatars">
-          <Avatar name="avatar1" onDrop={(product) => handleProductDrop('avatar1', product)} />
-          <Avatar name="avatar2" onDrop={(product) => handleProductDrop('avatar2', product)} />
-        </div>
+        <div className="row">
+          {Array.from({ length: avatarCount }).map((_, avatarIndex) => (
+            <div className="col-6" key={`avatar-column-${avatarIndex}`}>
+              <Avatar key={`avatar${avatarIndex}`} name={`avatar${avatarIndex}`} onDrop={(product) => handleProductDrop(avatarIndex, product)} />
 
-        <div className="avatar-products">
-          <div>
-            <h3>Avatar 1 Ürünleri</h3>
-            <ul>
-              {avatarProducts.avatar1.map((product, index) => (
-                <li key={index}>{product.name} - {product.price} TL</li>
-              ))}
-            </ul>
+              <div>
+                <h4>Avatar {avatarIndex + 1} Ürünleri</h4>
+                <ul>
+                  {avatarProducts[avatarIndex].map((product, productIndex) => (
+                    <li key={productIndex}>{product.name} - {product.price} TL</li>                
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
           </div>
-          <div>
-            <h3>Avatar 2 Ürünleri</h3>
-            <ul>
-              {avatarProducts.avatar2.map((product, index) => (
-                <li key={index}>{product.name} - {product.price} TL</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </DndProvider>
-
+          </DndProvider>
+        </>
+      )}
     </div>
   );
 }
